dfce0c183b12de63f58c5f7021fd5f9cad7ff652
bhyn98 <bhyn9785@gmail.com>
2022-08-09 16:37:17 +0900

    [FEAT] init commit

---
+++ clone_projects/clone_projects/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  clone_projects
+//
+//  Created by 강보현 on 2022/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("init")
+
+    }
+
+
+}
+
