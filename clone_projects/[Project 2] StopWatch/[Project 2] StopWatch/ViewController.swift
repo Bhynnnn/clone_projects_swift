835ad080ab700b8d31e4765a6ba56ff781ef34ee
bhyn98 <bhyn9785@gmail.com>
2022-08-09 16:50:28 +0900

    [FEAT] Create Project 2

---
+++ clone_projects/[Project 2] StopWatch/[Project 2] StopWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  [Project 2] StopWatch
+//
+//  Created by 강보현 on 2022/08/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello")
+    }
+
+
+}
+
