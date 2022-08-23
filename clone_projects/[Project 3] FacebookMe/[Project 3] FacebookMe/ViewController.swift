266927d28c8e0a1c10790db8e854e8675c8ab165
bhyn98 <bhyn9785@gmail.com>
2022-08-23 14:32:21 +0900

    [FEAT] init commit

---
+++ clone_projects/[Project 3] FacebookMe/[Project 3] FacebookMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  [Project 3] FacebookMe
+//
+//  Created by 강보현 on 2022/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("init")
+    }
+
+
+}
+
